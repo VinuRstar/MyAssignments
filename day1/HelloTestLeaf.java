32a98034f9aa58522524a302ec7564722deebe9d
VinuRstar <venkat.vinu@gmail.com>
2024-09-10 19:14:35 +0530

    Add files via upload
    week1.day1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	
+//main method ctrl space
+	
+	
+	public static void main(String[] args) {
+		
+		//syso ctrl space
+		
+		System.out.println("Welcome to Testleaf");
+	}
+}
+
+
