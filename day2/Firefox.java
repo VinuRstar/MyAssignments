12f50b256f34bd63525ef46cfb37a786fffac5fb
VinuRstar <venkat.vinu@gmail.com>
2024-09-10 19:15:14 +0530

    Add files via upload
    week1.day2

---
+++ day2/Firefox.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		//System.out.println(browserVersion+" "+browserName+" "+isVisible+" "+releaseYear+" "+browserLogo);
+		
+	    System.out.println("Browser Version - "+browserVersion);
+	    System.out.println("Browser Name - "+browserName);
+	    System.out.println("isVisible - "+isVisible);
+	    System.out.println("Release Year - "+releaseYear);
+	    System.out.println("BrowserLogo - "+browserLogo);
+
+		
+	}
+
+}
