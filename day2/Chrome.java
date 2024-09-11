5298f8758abd366653abda3f349103ae4d790b05
VinuRstar <venkat.vinu@gmail.com>
2024-09-11 19:01:39 +0530

    Add files via upload
    week1.day2.homeassignment

---
+++ day2/Chrome.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		float version = 91.0f;
+		String name = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		//System.out.println(name+" "+version+" "+isBeta+" "+releaseYear+" "+shortcutKey);
+		System.out.println("Name - "+name);
+		System.out.println("Version - "+version);
+		System.out.println("Is it a Beta - "+isBeta);
+		System.out.println("Release Year - "+releaseYear);
+		System.out.println("Shortcut Key is - "+shortcutKey);
+		
+		
+		
+	}
+
+}
